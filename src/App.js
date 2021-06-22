73384bd2993713c8dba1427024f51219a501396f
vladovpro <vladovpro@gmail.com>
2021-06-22 18:16:39 +0200

    Test CI/CD

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Shopyfi free shipping extension
         </a>
       </header>
     </div>
